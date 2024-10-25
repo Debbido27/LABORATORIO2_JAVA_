f0b9280097080f97fc9534120362181b0a3fd6af
Debbido27 <Davidmedina272004@gmail.com>
2024-10-25 08:33:24 -0600

    Se llama al scanner, y se inicia el menu, en donde el usuario podra ingresar la opcion que desea

---
+++ MEDINA_DAVID_CADENAS_/src/medina_david_cadenas_/MEDINA_DAVID_CADENAS_.java
@@ -0,0 +1,30 @@
+/*
+Menú
+
+Crear un menú que permita tener referencias de los ejercicios, y permita ingresar
+a cada uno y volver a retornar al menú hasta que indique la opción de salir.
+
+1. CADENA 
+
+Se ingresa una cadena del teclado y al final se imprime cuál es el carácter que más veces se repite en dicha cadena.
+
+La salida de pantalla debe mostrar el carácter y la cantidad de veces que se repite. Usar While
+ */
+package medina_david_cadenas_;
+
+import java.util.Scanner;
+
+public class MEDINA_DAVID_CADENAS_ {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("BIENVENIDO A MENU DE EJERCICIOS\n Ingrese el ejercicio que desea entrada\n1.Cadenas.\n2.Notas.\n3.CLases.\n4.Salir.\nDigite la opcion: "+"\n");
+        int Op1 = entrada.nextInt();
+        
+
+    }
+    
+}
