0a30d86a851f75e27ca2a2ce12a8d7fc2dde640c
Debbido27 <Davidmedina272004@gmail.com>
2024-10-25 08:12:52 -0600

    Se inicia el codigo, con el scanner, y se importa para poder leer las opciones del ususario

---
+++ MEDINA_DAVID_CADENA/src/medina_david_cadena/MEDINA_DAVID_CADENA.java
@@ -0,0 +1,30 @@
+
+package medina_david_cadena;
+
+import java.util.Scanner;
+
+
+public class MEDINA_DAVID_CADENA {
+
+    /*
+     Menú
+
+Crear un menú que permita tener referencias de los ejercicios, y permita ingresar a cada uno y volver a retornar al menú hasta que indique la opción de salir.
+
+1. CADENA 
+
+Se ingresa una cadena del teclado y al final se imprime cuál es el carácter que más veces se repite en dicha cadena.
+
+La salida de pantalla debe mostrar el carácter y la cantidad de veces que se repite. Usar While
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada= new Scanner(System.in);
+        
+        
+        //Menu 
+        
+    }
+    
+}
